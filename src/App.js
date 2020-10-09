e5dd1cbb1cbe925a27eaa8f7198c27475103a739
Willem Green <willberforce15@gmail.com>
2020-10-09 13:15:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Header from './Header/Header';
+import SearchForm from './SearchForm/SearchForm';
+import Results from './Results/Results'
+import { waitForDomChange } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    display:[],
+    error:null
+  }
+
+  handleGetResults = (searchValue, searchType) => {
+    const url = `https://swapi-thinkful.herokuapp.com/api/${searchType}/?search=${searchValue}`
+
+    fetch(url)
+    .then(res =>{
+      if(!res.ok) {
+        throw new Error('Something went wrong, please try again later')
+      }
+      return res
+    })
+    .then(res => res.json())
+    .then(data =>{
+      let newDisplay = data.results.map(item =>{
+        return {names:item.name}
+      })
+      this.setState({display:newDisplay})
+    })
+    .catch(err =>{
+      this.setState({error:err.message})
+      console.log(err)
+    })
+  }
+
+  render(){
+    return (
+      undefined
+    );
+  }
 }
 
 export default App;
